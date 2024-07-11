bb5bb78b5c777c92a1a013beaa9ef410aa1968f5
NikolozBeqauri <123400633+NikolozBeqauri@users.noreply.github.com>
2024-07-11 14:44:35 +0400

    reusable search component

---
+++ app/components/Search/Search.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+import styles from "./Search.module.scss";
+type Props = {
+    placeHolder?: string;
+}
+
+export const Search = ({ placeHolder }: Props) => {
+    placeHolder = placeHolder ? placeHolder : "Artists, tracks, albums"
+
+    return (
+        <form className={styles.searchform}>
+
+            <input type="text" placeholder={placeHolder} />
+            <Image
+                src="/icons/search.svg"
+                alt="search icon"
+                width={24}
+                height={24}
+            />
+        </form>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import colorsEnum from "./enums/colors.enum";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main style = {{color : colorsEnum.Background}}>
-
+    <main >
     </main>
   );
 }
