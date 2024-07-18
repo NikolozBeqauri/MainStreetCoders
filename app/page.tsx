8cfffca53b6abcdcf7380d9bfe7ad03c7b0b049a
iliaGobejishvili <158037628+iliaGobejishvili@users.noreply.github.com>
2024-07-18 16:09:55 +0400

    feat:added table structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+'use client'
+import { App, Table } from "antd";
 import styles from "./page.module.css";
+import ReasubleTable from "./components/ReusableTable/ReasubleTable";
 
 export default function Home() {
   return (
     <main>
+      <ReasubleTable/>
     </main>
   );
 }
