cfe4121e76d9121c5f6f4e78c138037651b4b067
TepnadzeS1 <71930811+TepnadzeS1@users.noreply.github.com>
2024-07-24 18:41:20 +0400

    feat: created audio player

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+'use client'
+
+import AudioPlayer from "./components/AudioPlayer/AudioPlayer";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main>
+      <AudioPlayer />
     </main>
   );
 }
