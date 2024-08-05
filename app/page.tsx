1302a47e65fa567257de4b20d577621d9093c66b
iliaGobejishvili <158037628+iliaGobejishvili@users.noreply.github.com>
2024-08-05 16:19:45 +0400

    fix: fixed structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,12 @@
+'use client'
+import { ReusableTable } from "./components/ReusableTable/Reusable";
+
+export default function Home() {
+  return (
+    <main>
+
+    <ReusableTable/>
+    </main>
+  );
+}
 
