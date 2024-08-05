f88dc52503f810bed869be263901bc5804f52c61
TepnadzeS1 <71930811+TepnadzeS1@users.noreply.github.com>
2024-08-05 15:41:25 +0400

    feat: updated Top Hit Page

--- app/components/TopHitPage/TopHitPage.tsx
+++ app/components/TopHitPage/TopHitPage.tsx
@@ -1,14 +1,14 @@
 
+import { Header } from '../Header/Header'
 import { NewsComponent } from '../NewsComponent/NewsComponent'
 import { Search } from '../Search/Search'
-import { HeaderOfHitPage } from './subcomponent/HeaderOfHitPage'
 import './TopHitPage.scss'
 
 export const TopHitPage = () => {
 
     return (
         <div className='contentOfHit'>
-            <HeaderOfHitPage />
+            <Header imgName='rightArrow'/>
             <NewsComponent title='Top Hit  Of the week' count={999} />
             <div className='contentOfSongs'>
                 <Search />
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { SideBar } from "./components/SideBar/SideBar";
+import { TopHitPage } from "./components/TopHitPage/TopHitPage";
 
 export default function Home() {
   return (
     <main>
-      <SideBar/>
+      {/* <SideBar/> */}
+      <TopHitPage />
     </main>
   );
 }
