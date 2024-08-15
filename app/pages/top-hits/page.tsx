d3593830de3fae66ff2b8531b0608ddccf8872f6
TepnadzeS1 <71930811+TepnadzeS1@users.noreply.github.com>
2024-08-15 17:02:52 +0400

    ref: audio Player and edit media query 744px

--- app/pages/top-hits/page.tsx
+++ app/pages/top-hits/page.tsx
@@ -1,9 +1,23 @@
-const TopHitsPage = () => {
-    return(
-        <div>
-            tophits
+import { Header } from '../../components/Header/Header'
+import { NewsComponent } from '../../components/NewsComponent/NewsComponent'
+import { ReusableTable } from '../../components/ReusableTable/Reusable'
+import { Search } from '../../components/Search/Search'
+import styles from './page.module.scss'
+
+const TopHitPage = () => {
+
+    return (
+        <div className={styles.contentOfHit}>
+            <div className={styles.headerMover}>
+                <Header imgName='rightArrow'/>
+            </div>
+            <NewsComponent title='Top Hit  Of the week' count={'999'} />
+            <div className={styles.contentOfSongs}>
+                <Search />
+                <ReusableTable />
+            </div>
         </div>
     )
 }
 
-export default TopHitsPage;
+export default TopHitPage;
