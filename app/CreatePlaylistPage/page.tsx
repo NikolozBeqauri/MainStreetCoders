5a8c48dc83e7560184b001f1a07e68667d81fb42
BeqaTefnadze <beqatepnadze@gmail.com>
2024-08-20 22:02:39 +0400

    fix: double sidebar problem

--- app/CreatePlaylistPage/page.tsx
+++ app/CreatePlaylistPage/page.tsx
@@ -5,20 +5,21 @@ import { Header } from "../components/Header/Header"
 import ReusableButton from "../components/ReusableButton/ReusableButton";
 import { Search } from "../components/Search/Search";
 import { SideBar } from "../components/SideBar/SideBar"
+import { UserPlaylist } from "../components/UserPlaylist/UserPlaylist";
 import Styles from "./page.module.scss";
 
 const CreatePlaylistPage = () => {
 
     return(
         <div className={Styles.container}>
-        <SideBar />
             <div className={Styles.childrenContainer}>
-                <Header />
+                <Header imgName={"icon"}/>
                 <h1 className={Styles.header}>My Playlists</h1>
                 <div className={Styles.searchLayout}>
                     <Search />
                     <ReusableButton title={"New Playlist"} />
                 </div>
+                <UserPlaylist image={"ad"} count={12} />
             </div>
             
         </div>
