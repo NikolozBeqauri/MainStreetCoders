d82596cb8beff6c10af87e24a49b2d76e9cafba4
NikolozBeqauri <123400633+NikolozBeqauri@users.noreply.github.com>
2024-08-20 12:58:23 +0400

    feat: added burger menu fetcher

--- app/components/MobileNavbar/MobileNavbarLinks/MobileNavbarLinks.tsx
+++ app/components/MobileNavbar/MobileNavbarLinks/MobileNavbarLinks.tsx
@@ -1,6 +1,6 @@
 export const menuItems = [
     { id: 0, label: 'Home', icon: '/icons/menuHome.svg', activeIcon: '/icons/menuHomeActive.svg' },
-    { id: 1, label: 'Search', icon: '/icons/menuSearch.svg', activeIcon: '/icons/menuSearch.svg' },
+    { id: 1, label: 'Search', icon: '/icons/menuSearch.svg', activeIcon: '/icons/menuSearchActive.svg' },
     { id: 2, label: 'My Playlist', icon: '/icons/menuPlaylists.svg', activeIcon: '/icons/menuPlaylistsActive.svg' },
 ];
 
--- app/components/SideBar/SideBar.tsx
+++ app/components/SideBar/SideBar.tsx
@@ -21,11 +21,8 @@ export const SideBar = () => {
         const handleResize = () => {
             setactiveSidebar(window.innerWidth >= 745);
         };
-
         handleResize();
-
         window.addEventListener('resize', handleResize);
-
         () => window.removeEventListener('resize', handleResize);
     }, [setactiveSidebar]);
 
@@ -34,83 +31,89 @@ export const SideBar = () => {
 
     const sidebarGeneralStyles = [styles.sideBarWrapper]
     return (
-        <aside className={sidebarGeneralStyles.join(" ").trim()} >
-            <div className={styles.mainLogo}
-                onClick={() => router.push('/')}>
-                <Image
-                    src={`/images/mainLogo.png`}
-                    alt="main logo"
-                    width={98}
-                    height={83}
-                    tabIndex={0}
-                />
-            </div>
-            <nav className={styles.sideBarNav}>
-                <div className={styles.generalLinks}>
-                    {GeneralLinks.map(link => (
-                        <div
-                            className={styles.navBarlink}
-                            key={link.label}
-                            onClick={() => {
-                                setActive(link.label)
-                                router.push(`${link.href}`)
-                            }}
-                            onFocus={() => setActive(link.label)}
-                            onBlur={() => setActive(null)}
-                            id={active === link.label ? styles.active : ""}
-                        >
-                            <ReusableIcon
-                                imgName={link.imageName}
-                                active={active === link.label}
-                                onFocus={() => setActive(link.label)}
-                            />
-                            <span>{link.label}</span>
-                        </div>
-                    ))}
-                    <h2>Collection</h2>
-                    {Colectionlinks.map(link => (
-                        <div
-                            className={styles.navBarlink}
-                            key={link.label}
-                            onClick={() => {
-                                setActive(link.label)
-                                router.push(`${link.href}`)
-                            }}
-                            onFocus={() => setActive(link.label)}
-                            onBlur={() => setActive(null)}
-                            id={active === link.label ? styles.active : ""}
-                        >
-                            <ReusableIcon
-                                imgName={link.imageName}
-                                active={active === link.label}
-                                onFocus={() => setActive(link.label)}
-                            />
-                            <span>{link.label}</span>
-                        </div>
-                    ))}
-                    <h2>Discover</h2>
-                    {Discoverlinks.map(link => (
-                        <div
-                            className={styles.navBarlink}
-                            key={link.label}
-                            onClick={() => {
-                                setActive(link.label)
-                                router.push(`${link.href}`)
-                            }}
-                            onFocus={() => setActive(link.label)}
-                            onBlur={() => setActive(null)}
-                            id={active === link.label ? styles.active : ""}
-                        >
-                            <ReusableIcon
-                                imgName={link.imageName}
-                                active={active === link.label}
-                                onFocus={() => setActive(link.label)}
-                            />
-                            <span>{link.label}</span>
-                        </div>
-                    ))}
+        <div>
+            <div className={styles.toDisableMenuDiv}
+                onClick={() => setactiveSidebar(false)}
+            ></div>
+            <aside className={sidebarGeneralStyles.join(" ").trim()} >
+                <div className={styles.mainLogo}
+                    onClick={() => router.push('/')}>
+                    <Image
+                        src={`/images/mainLogo.png`}
+                        alt="main logo"
+                        width={98}
+                        height={83}
+                        tabIndex={0}
+                    />
                 </div>
-            </nav>
-        </aside >
+                <nav className={styles.sideBarNav}>
+                    <div className={styles.generalLinks}>
+                        {GeneralLinks.map(link => (
+                            <div
+                                className={styles.navBarlink}
+                                key={link.label}
+                                onClick={() => {
+                                    setActive(link.label)
+                                    router.push(`${link.href}`)
+                                }}
+                                onFocus={() => setActive(link.label)}
+                                onBlur={() => setActive(null)}
+                                id={active === link.label ? styles.active : ""}
+                            >
+                                <ReusableIcon
+                                    imgName={link.imageName}
+                                    active={active === link.label}
+                                    onFocus={() => setActive(link.label)}
+                                />
+                                <span>{link.label}</span>
+                            </div>
+                        ))}
+                        <h2>Collection</h2>
+                        {Colectionlinks.map(link => (
+                            <div
+                                className={styles.navBarlink}
+                                key={link.label}
+                                onClick={() => {
+                                    setActive(link.label)
+                                    router.push(`${link.href}`)
+                                }}
+                                onFocus={() => setActive(link.label)}
+                                onBlur={() => setActive(null)}
+                                id={active === link.label ? styles.active : ""}
+                            >
+                                <ReusableIcon
+                                    imgName={link.imageName}
+                                    active={active === link.label}
+                                    onFocus={() => setActive(link.label)}
+                                />
+                                <span>{link.label}</span>
+                            </div>
+                        ))}
+                        <h2>Discover</h2>
+                        {Discoverlinks.map(link => (
+                            <div
+                                className={styles.navBarlink}
+                                key={link.label}
+                                onClick={() => {
+                                    setActive(link.label)
+                                    router.push(`${link.href}`)
+                                }}
+                                onFocus={() => setActive(link.label)}
+                                onBlur={() => setActive(null)}
+                                id={active === link.label ? styles.active : ""}
+                            >
+                                <ReusableIcon
+                                    imgName={link.imageName}
+                                    active={active === link.label}
+                                    onFocus={() => setActive(link.label)}
+                                />
+                                <span>{link.label}</span>
+                            </div>
+                        ))}
+                    </div>
+                </nav>
+            </aside >
+        </div>
+
     );
 };
