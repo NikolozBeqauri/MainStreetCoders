f95817b549e29ad0f35cca6a32bb97e2dc0d510c
NikolozBeqauri <123400633+NikolozBeqauri@users.noreply.github.com>
2024-08-24 18:25:46 +0400

    feat: added sign up page

--- app/pages/signup/page.tsx
+++ app/pages/signup/page.tsx
@@ -1,7 +1,25 @@
 import { SignUp } from "@/app/components/SighnUp/SignUp";
+import styles from './page.module.scss'
+import Image from "next/image";
 
 export const SighnUpPage = () => {
-    return(
+    return (
+        <div className={styles.signUpWrapper}>
+            <Image
+                src={'/images/mainLogo.png'}
+                alt="icon"
+                width={98}
+                height={83}
+            />
+            <div className={styles.content}>
+                <div className={styles.titles}>
+                    <h2 className={styles.where}>WHERE <span className={styles.harmony}>HARMONY</span></h2>
+                    <h2 className={styles.meets}>MEETS <span className={styles.melody}>MELODY</span></h2>
+                    <p>The Future Of Music Streaming</p>
+                </div>
+                <SignUp/>
+            </div>
+        </div>
     )
 }
 
