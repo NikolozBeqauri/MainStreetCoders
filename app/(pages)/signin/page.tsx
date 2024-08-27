451fbe95028ac06d9b973990f177e0ec0580dfbf
NikolozBeqauri <123400633+NikolozBeqauri@users.noreply.github.com>
2024-08-27 14:50:30 +0400

    feat: created sign in page

---
+++ app/(pages)/signin/page.tsx
@@ -0,0 +1,11 @@
+import SignPage from "@/app/components/SignPage/SignPage";
+
+export const SighnInPage = () => {
+    return (
+        <>
+            <SignPage component={"SignIn"}/>
+        </>
+    )
+}
+
+export default SighnInPage;
--- app/components/SignIn/SignIn.tsx
+++ app/components/SignIn/SignIn.tsx
@@ -1,10 +1,56 @@
 'use client'
-import styles from "./SignUp.module.scss"
+import { useForm } from "react-hook-form";
+import styles from "./SignIn.module.scss"
+type FormValues = {
+    email: string;
+    password: string;
+    chackbox: boolean;
+};
 
 export const SignIn = () => {
-    return (
-        <form>
+    const { register, handleSubmit, formState: { errors } } = useForm<FormValues>();
 
+    const onSubmit = (data: FormValues) => {
+        console.log(data);
+    };
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)} className={styles.fromWrapper}>
+            <input
+                placeholder="Email"
+                type="email"
+                {...register("email", { required: "Email is required" })}
+            />
+            {errors.email && <span className={styles.errorMessage}>{errors.email.message}</span>}
+
+            <input
+                placeholder="Password"
+                type="password"
+                {...register("password", {
+                    required: "Password is required",
+                    minLength: {
+                        value: 8,
+                        message: "Password must be at least 8 characters"
+                    },
+                    validate: {
+                        hasUpperCase: value => /[A-Z]/.test(value) || "Password must contain at least one uppercase letter",
+                        hasNumber: value => /\d/.test(value) || "Password must contain at least one number"
+                    }
+                })}
+            />
+            {errors.password && <span className={styles.errorMessage}>{errors.password.message}</span>}
+
+            <div className={styles.memoryWrapper}>
+                <div className={styles.checkbox}>
+                    <input type="checkbox" id="remember" {...register("chackbox")} />
+                    <label htmlFor="remember">Remember me</label>
+                </div>
+                <span className={styles.forgotPassword}>Forgot your password?</span>
+            </div>
+
+            <input type="submit" value="Sign Up" />
+
+            <p className={styles.haveAccount} >Already have  an account? <span>Sign in</span></p>
         </form>
     );
 };
