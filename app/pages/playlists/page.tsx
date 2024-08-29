afb4593501b20ce270e558e202dbab84b5c5fd26
BeqaTefnadze <beqatepnadze@gmail.com>
2024-08-29 17:51:13 +0400

    fix: page routing

--- app/pages/playlists/page.tsx
+++ app/pages/playlists/page.tsx
@@ -1,8 +1,27 @@
+import AudioPlayer from "../../components/AudioPlayer/AudioPlayer";
+import { Header } from "../../components/Header/Header"
+import ReusableButton from "../../components/ReusableButton/ReusableButton";
+import { Search } from "../../components/Search/Search";
+import { SideBar } from "../../components/SideBar/SideBar"
+import { UserPlaylist } from "../../components/UserPlaylist/UserPlaylist";
+import Styles from "./page.module.scss";
+
 const PlayListPage = () => {
     return(
-        <div>
-            play lists
+        
+            <div className={Styles.container}>
+            <div className={Styles.childrenContainer}>
+                <Header imgName={"icon"}/>
+                <h1 className={Styles.header}>My Playlists</h1>
+                <div className={Styles.searchLayout}>
+                    <Search />
+                    <ReusableButton title={"New Playlist"} />
+                </div>
+                <UserPlaylist image={"ad"} count={12} />
+            </div>
+            
         </div>
+        
     )
 }
 
