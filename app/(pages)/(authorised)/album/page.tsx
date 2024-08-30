a5c2ac9a946ab5e28fa2f608e71678b66be36af5
iliaGobejishvili <158037628+iliaGobejishvili@users.noreply.github.com>
2024-08-30 17:07:34 +0400

    feat:responsive album page

--- app/(pages)/(authorised)/album/page.tsx
+++ app/(pages)/(authorised)/album/page.tsx
@@ -1,7 +1,22 @@
+import { Header } from "@/app/components/Header/Header";
+import { NewsComponent } from "@/app/components/NewsComponent/NewsComponent";
+import { ReusableTable } from "@/app/components/ReusableTable/Reusable";
+import { Search } from "@/app/components/Search/Search";
+import styles from "./page.module.scss"
+import { BurgerMenu } from "@/app/components/BurgerMenu/BurgerMenu";
 const AlbumPage = () => {
-    return(
-        <div>
-            album
+    return (
+        <div style={{
+            width: "100%"
+        }}>
+            <div className={styles.album1}>
+                <Header />
+                <NewsComponent title={"Seek For Marktoop"} count={"Released 07/12/2023"} />
+            </div>
+            <div className={styles.album2}>
+            <ReusableTable />
+            </div>
+            <BurgerMenu/>
         </div>
     )
 }
