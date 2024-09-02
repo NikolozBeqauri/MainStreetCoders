cc6ae9e391b829e8b74a375c51ee5374da954284
iliaGobejishvili <158037628+iliaGobejishvili@users.noreply.github.com>
2024-09-02 14:01:31 +0400

    feat: create resposive albumpage

--- app/(pages)/(authorised)/album/page.tsx
+++ app/(pages)/(authorised)/album/page.tsx
@@ -6,17 +6,19 @@ import styles from "./page.module.scss"
 import { BurgerMenu } from "@/app/components/BurgerMenu/BurgerMenu";
 const AlbumPage = () => {
     return (
-        <div style={{
-            width: "100%"
-        }}>
-            <div className={styles.album1}>
-                <Header />
-                <NewsComponent title={"Seek For Marktoop"} count={"Released 07/12/2023"} />
-            </div>
+        <div className={styles.album}>
             <div className={styles.album2}>
-            <ReusableTable />
+                <div className={styles.album1}>
+                    <BurgerMenu />
+                    <Header />
+                </div>
+                <div> 
+                    <NewsComponent title={"Seek For Marktoop"} count={"Released 07/12/2023"} />
+                    <div className={styles.table}>
+                    <ReusableTable />
+                    </div>
+                </div>
             </div>
-            <BurgerMenu/>
         </div>
     )
 }
