14ff8e5e461ec0069cb1bff622c28744f59e2c60
NikolozBeqauri <123400633+NikolozBeqauri@users.noreply.github.com>
2024-09-03 15:38:17 +0400

    feat: general structure and responsive (768px)

---
+++ app/(pages)/(authorised)/artist/artistPageData/artistPageData.tsx
@@ -0,0 +1,21 @@
+export const artistPageData = [
+    { author: 'Anyma',  img: 'taylorSwift' },
+    { author: 'Beyonce',  img: 'billieEilish' },
+    { author: 'Anetha',  img: 'sza' },
+    { author: 'Boris Brejcha', img: 'edSheeran' },
+
+    { author: 'Anyma',  img: 'taylorSwift' },
+    { author: 'Beyonce',  img: 'billieEilish' },
+    { author: 'Anetha',  img: 'sza' },
+    { author: 'Boris Brejcha', img: 'edSheeran' },
+
+    { author: 'Anyma',  img: 'taylorSwift' },
+    { author: 'Beyonce',  img: 'billieEilish' },
+    { author: 'Anetha',  img: 'sza' },
+    { author: 'Boris Brejcha', img: 'edSheeran' },
+
+    { author: 'Anyma',  img: 'taylorSwift' },
+    { author: 'Beyonce',  img: 'billieEilish' },
+    { author: 'Anetha',  img: 'sza' },
+    { author: 'Boris Brejcha', img: 'edSheeran' },
+]
--- app/(pages)/(authorised)/artist/page.tsx
+++ app/(pages)/(authorised)/artist/page.tsx
@@ -1,7 +1,23 @@
+import { Header } from "@/app/components/Header/Header";
+import styles from "./page.module.scss"
+import { artistPageData } from "./artistPageData/artistPageData";
+import { AlbumCard } from "@/app/components/AlbumCard/AlbumCard";
 const ArtistPage = () => {
-    return(
-        <div>
-            artists
+    return (
+        <div className={styles.artistPageWrapper}>
+            <Header />
+            <div className={styles.artistCardsWrapper}>
+                <h2>Trending Now</h2>
+                <div className={styles.artistCards}>
+                    {artistPageData.map((album, index) => (
+                        <AlbumCard
+                            key={index}
+                            author={album.author}
+                            img={album.img}
+                        />
+                    ))}
+                </div>
+            </div>
         </div>
     )
 }
