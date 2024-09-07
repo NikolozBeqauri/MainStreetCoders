9d31bd0322487f7d097e599bf24b0e39432d8b3c
BeqaTefnadze <beqatepnadze@gmail.com>
2024-09-07 17:51:30 +0400

    fix: playlist card

--- app/components/UserPlaylist/UserPlaylist.tsx
+++ app/components/UserPlaylist/UserPlaylist.tsx
@@ -1,15 +1,28 @@
+import { HeartIcon } from "../HeartIcon/HeartIcon";
+import { ReusableIcon } from "../ReusableIcon/ReusableIcon";
 import Styles from "./UserPlaylist.module.scss";
 
 type Props = {
     image: string;
     count?: number;
+    name?: string
 }
 
 export const UserPlaylist = (props: Props) => {
     return(
-        <div className={Styles.container}>
-            <img src={`/icons/${props.image}.svg`} alt="Playlist icon" width={234} height={251} />
-            <p className={Styles.paragraph}>{`Playlist name ${props.count}`}</p>
+        
+        <div className={`${Styles.container}`}>
+                <div className={Styles.containerBox}>
+                    <img src={`/images/${props.image}.png`} alt="Playlist icon" width={234} height={251} className={Styles.image}/>
+                    <div className={`${Styles.icons} ${Styles.opacity}`} >
+                        <HeartIcon background/>
+                        <ReusableIcon imgName={'threeDots'} background/>
+                    </div>
+                </div>
+            
+            <p className={Styles.paragraph}>{props.name}</p>
         </div>
+        
+
     )
 }
