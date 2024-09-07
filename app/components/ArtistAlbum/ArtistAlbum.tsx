1643cb7dfd4ac81f25fa90d2c092b01c721223d5
NikolozBeqauri <123400633+NikolozBeqauri@users.noreply.github.com>
2024-09-07 19:53:17 +0400

    feat: added nav logic

--- app/components/ArtistAlbum/ArtistAlbum.tsx
+++ app/components/ArtistAlbum/ArtistAlbum.tsx
@@ -1,7 +1,16 @@
+import { UserPlaylist } from "../UserPlaylist/UserPlaylist"
+import artistAlbumData from "./artistAlbumData/artistAlbumData"
+import styles from "./ArtistAlbum.module.scss"
 export const ArtistAlbum = () => {
-    return(
-        <div>
-            artistAlbum
+    return (
+        <div className={styles.artistAlbumWrapper}>
+            {artistAlbumData.map((album, index) => (
+                <UserPlaylist
+                    key={index}
+                    name={album.title}
+                    image={album.image} 
+                />
+            ))}
         </div>
     )
 }
