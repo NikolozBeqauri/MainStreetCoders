882fb870915cd12800cb1e38a2d21fafac8081d1
iliaGobejishvili <158037628+iliaGobejishvili@users.noreply.github.com>
2024-09-09 13:47:00 +0400

    feat/ create recommendation page

--- app/(pages)/(authorised)/recommendations/page.tsx
+++ app/(pages)/(authorised)/recommendations/page.tsx
@@ -1,11 +1,20 @@
-import { AlbumCard } from "@/app/components/AlbumCard/AlbumCard";
-import styles from "./page.module.scss"
-import { toprecomentadtiondata } from "@/app/components/AlbumCard/toprecomentadtiondata/TopReommendationsData";
-const RecommendaionPage  = () => {
-    return (
-        <section className={styles.generalCardWrapper}>
+import { AlbumCard } from '@/app/components/AlbumCard/AlbumCard';
+import styles from './page.module.scss'
+import { toprecomentadtiondata } from '@/app/components/AlbumCard/toprecomentadtiondata/TopReommendationsData';
+import { AlbumsYouMightLove } from '@/app/components/AlbumCard/toprecomentadtiondata/AlbumsYouMightLove';
+import { BecauseyoulistenedtoPeggyGou } from '@/app/components/AlbumCard/toprecomentadtiondata/BecauseyoulistenedtoPeggyGou';
+import { HiddenGems } from '@/app/components/AlbumCard/toprecomentadtiondata/HiddenGems';
+import { Header } from '@/app/components/Header/Header';
+
+
+const RecommendaionPage = () => {
+  return (
+  
+    <div className={styles.recomendationPage}>
+      <Header/>
+      <section className={styles.generalCardWrapper}>
         <div className={styles.secondaryTitleDiv}>
-          <h2>Top Hits</h2>
+          <h2>Top Recommendations</h2>
           <span>See all</span>
         </div>
         <div className={styles.generalCardItem}>
@@ -19,7 +28,60 @@ const RecommendaionPage  = () => {
           ))}
         </div>
       </section>
-    )
+
+      <section className={styles.generalCardWrapper}>
+        <div className={styles.secondaryTitleDiv}>
+          <h2>Albums You Might Love</h2>
+          <span>See all</span>  
+        </div>
+        <div className={styles.generalCardItem}>
+          {AlbumsYouMightLove.map((album, index) => (
+            <AlbumCard
+              key={index}
+              author={album.author}
+              title={album.title}
+              img={album.img}
+            />
+          ))}
+        </div>
+      </section>
+      <section className={styles.generalCardWrapper}>
+        <div className={styles.secondaryTitleDiv}>
+          <h2>Because you listened to Peggy Gou </h2>
+          <span>See all</span>
+        </div>
+        <div className={styles.generalCardItem}>
+          {BecauseyoulistenedtoPeggyGou.map((album, index) => (
+            <AlbumCard
+              key={index}
+              author={album.author}
+              img={album.img}
+            />
+          ))}
+        </div>
+      </section>
+
+      <section className={styles.generalCardWrapper}>
+        <div className={styles.secondaryTitleDiv}>
+          <h2>Hidden Gems</h2>
+          <span>See all</span>
+        </div>
+        <div className={styles.generalCardItem}>
+          {HiddenGems.map((album, index) => (
+            <AlbumCard
+              key={index}
+              author={album.author}
+              title={album.title}
+              img={album.img}
+            />
+          ))}
+        </div>
+      </section>
+
+    </div>
+  )
+
 }
+
 
 export default RecommendaionPage;
---
+++ app/components/AlbumCard/toprecomentadtiondata/HiddenGems.tsx
@@ -0,0 +1,7 @@
+export const HiddenGems = [
+    { author: 'Of Monsters And Men',title: 'Fever Dream',  img: 'HiddenGemsOfMontersAndMen' },
+    { author: 'Lenny Kravitz', title: 'Blue Electric Light', img: 'HiddenGemsLennyKravits' },
+    { author: 'Dua Lipa', title: 'Radical Optimism', img: 'HiddenGemsDuaLipa' },
+    { author: 'Love Lo',title: 'Heat', img: 'HiddenGemsLoveLo' },
+
+]
