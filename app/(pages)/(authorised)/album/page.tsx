c88ede7a3b28477a5393c5d938dadf767a4d4ff2
TepnadzeS1 <71930811+TepnadzeS1@users.noreply.github.com>
2024-10-12 19:04:39 +0400

    created: album connections

--- app/(pages)/(authorised)/album/page.tsx
+++ app/(pages)/(authorised)/album/page.tsx
@@ -5,41 +5,10 @@ import { NewsComponent } from "@/app/components/NewsComponent/NewsComponent";
 import styles from "./page.module.scss";
 import { BurgerMenu } from "@/app/components/BurgerMenu/BurgerMenu";
 import { ReusableTable } from "@/app/components/ReusableTable/ReusableTable";
-import { useEffect, useState } from "react";
-import axios from "axios";
-import Loading from "@/app/components/Loading/Loading";
-import Cookies from 'js-cookie';
+import { useState } from "react";
 
 const AlbumPage = () => {
-  const [albumId, setAlbumId] = useState(""); 
-  const [loading, setLoading] = useState(true); 
-  const token = Cookies.get("token");
-
-  useEffect(() => {
-    axios
-      .get("https://project-spotify-1.onrender.com/albums", {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-      .then((response) => {
-        const data = response.data;
-        if (data && data.length > 0) {
-          setAlbumId(data[0].id); 
-        } else {
-          console.error("No albums found.");
-        }
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error("Error fetching albums:", error);
-        setLoading(false);
-      });
-  }, [token]);
-
-  if (loading) {
-    return <Loading width="" />;
-  }
+  const [audioPlayerData, setAudioPlayerData] = useState<any | null>(null);
 
   return (
     <div className={styles.album}>
@@ -55,7 +24,7 @@ const AlbumPage = () => {
             image={"artistDemoImage"}
           />
           <div className={styles.table}>
-            <ReusableTable pageName="albums" id={albumId} />
+            <ReusableTable pageName="albums" setAudioPlayerData={setAudioPlayerData} />
           </div>
         </div>
       </div>
