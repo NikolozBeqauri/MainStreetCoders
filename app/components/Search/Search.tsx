b5424f9b876daf43dfc8df72c2416a353b9ea585
BeqaTefnadze <beqatepnadze@gmail.com>
2024-10-14 16:54:23 +0400

    feat: search bar

--- app/components/Search/Search.tsx
+++ app/components/Search/Search.tsx
@@ -1,23 +1,48 @@
 import Image from 'next/image';
 import styles from "./Search.module.scss";
+import axios from 'axios';
+import Cookies from 'js-cookie';
+import { useState } from 'react';
+import { SearchSuggestions } from '../searchSuggestions/SearchSugestions';
 type Props = {
     placeHolder?: string;
 }
-
 export const Search = (props: Props) => {
+    const token = Cookies.get("token");
+    const [suggestions, setSuggestions] = useState<any>(null);
+    const [value, setValue] = useState<any>(null);
     
-    const placeHolder = props.placeHolder ? props.placeHolder : "Artists, tracks, albums"
+    
+    const placeHolder = props.placeHolder ? props.placeHolder : "Artists, tracks, albums";
+    const searchEngine = (e: any) => {
+        setValue(e.target.value);
+
+        axios.get(`https://project-spotify-1.onrender.com/search?searchfield=${value}`, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            }
+          })
+          .then((r) => {
+            console.log(r.data.music);
+            setSuggestions(r.data.music);
+          })
+    };
 
     return (
-        <div className={styles.searchform}>
-            <input type="text" placeholder={placeHolder} />
-            <Image 
-                className={styles.searchImage}
-                src="/icons/search.svg"
-                alt="search icon"
-                width={24}
-                height={24}
-            />
-        </div>
+        <>
+            <div 
+            className={styles.searchform}
+            onChange={searchEngine}>
+                <input type="text" placeholder={placeHolder} />
+                <Image 
+                    className={styles.searchImage}
+                    src="/icons/search.svg"
+                    alt="search icon"
+                    width={24}
+                    height={24}
+                />
+            </div>
+            {value && value.length != 0 && <SearchSuggestions suggestions={suggestions} />}
+        </>
     )
 }
